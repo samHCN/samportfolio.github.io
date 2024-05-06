618e65824585a53601883f76a2f936997be0471b
samHCN <134074847+samHCN@users.noreply.github.com>
2024-05-07 01:04:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ function addRecommendation() {
   let recommendation = document.getElementById("new_recommendation");
   // If the user has left a recommendation, display a pop-up
   if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
+    
 
-    // Create a new 'recommendation' element and set it's value to the user's message
+   // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
     element.setAttribute("class","recommendation");
     element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
